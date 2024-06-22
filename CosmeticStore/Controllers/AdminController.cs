e93965bcd290bf0e6aa485daef79439001ce77ac
Ивелин Танев <IvelinTanev.IT@gmail.com>
2024-06-22 22:22:34 +0300

    Added a admin panel log in functionality, with appropriate errors.

---
+++ CosmeticStore/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using BeautyCareStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautyCareStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<CustomUser> _userManager;
+
+        public AdminController(UserManager<CustomUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ValidatePrivileges(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                //ModelState.AddModelError(string.Empty, "Username is required.");
+                return View("Index");
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                //ModelState.AddModelError(string.Empty, "Password is required.");
+                return View("Index");
+            }
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                var isPasswordValid = await _userManager.CheckPasswordAsync(user, password);
+
+                if (isPasswordValid)
+                {
+                    // Password is correct, proceed with admin panel actions
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    // Handle invalid password
+                    //ModelState.AddModelError(string.Empty, "Invalid password.");
+                    return View("Index");
+                }
+            }
+            else
+            {
+                // Handle user not found
+                ModelState.AddModelError(string.Empty, "User not found.");
+                return View("Index");
+            }
+        }
+    }
+}
